0bf3aeb77afedff1b24434932f00606e458dd6de
Brent Emery <brent@Brents-MacBook-Pro.local>
2026-03-03 18:50:10 -0600

    Added pkg files

---
+++ crosswind/crosswind-wasm/pkg/crosswind_wasm_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const calculate_crosswind: (a: number, b: number, c: number) => number;
+export const calculate_headwind: (a: number, b: number, c: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
